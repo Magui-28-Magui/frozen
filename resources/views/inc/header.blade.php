9b15ec482ea2a3888d3048a4088a2d78382f18e0
Magui C <margarita.campos.lagunas@uabc.edu.mx>
2018-12-11 06:28:29 -0800

    blade

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -14,7 +14,7 @@
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
 
-	  <div class="collapse navbar-collapse" id="navbarColor01">
+	  <div class="collapse navbar-collapse justify-content-end" id="navbarColor01">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active">
 	        <a class="nav-link" href="{{ url('/')}}">Home <span class="sr-only">(current)</span></a>
